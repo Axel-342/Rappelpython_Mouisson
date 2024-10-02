7fd6053a9084c3cdd6ccc6be34ce969fc4d7ceb3
Axel-342 <axel.mouisson@gmail.com>
2024-10-02 16:46:15 +0200

    exercice 3

---
+++ 3.py
@@ -0,0 +1,10 @@
+nombre1 = int(input("Rentrez le premier nombre de la somme à calculer : "))
+nombre2 = int(input("Rentrez le second nombre de la somme à calculer : "))
+somme = nombre1 + nombre2
+print(somme)
+
+nom = input("Rentrez votre nom ici : ")
+print("Bonjour",nom,"ravi de faire votre connaissance")
+
+liste = [1,2,3,4,5,8,7]
+print(max(liste))
